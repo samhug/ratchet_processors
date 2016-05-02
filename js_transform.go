216dbecb0463611b3532aad84659921136864d9c
Sam Hug <samuel.b.hug@gmail.com>
2016-05-02 13:45:57 -0700

    Precompile JS code, and export a logging function

--- js_transform.go
+++ js_transform.go
@@ -15,14 +15,28 @@ type JSConfig struct {
 
 type JsTransform struct {
 	vm     *otto.Otto
-	script string
+	script *otto.Script
 }
 
-func NewJsTransform(script string) *JsTransform {
-	return &JsTransform{
-		vm:     otto.New(),
-		script: script,
+func NewJsTransform(script string) (*JsTransform, error) {
+
+	vm := otto.New()
+
+	// Add logging function
+	vm.Set("logWrite", func(call otto.FunctionCall) otto.Value {
+	    fmt.Printf("JS_LOG: %s\n", call.Argument(0).String())
+	    return otto.Value{}
+	})
+
+	vmScript, err := vm.Compile("", script)
+	if err != nil {
+		return nil, fmt.Errorf("Error compiling script: %s", err)
 	}
+
+	return &JsTransform{
+		vm:     vm,
+		script: vmScript,
+	}, nil
 }
 
 func (t *JsTransform) ProcessData(d data.JSON, outputChan chan data.JSON, killChan chan error) {
@@ -30,32 +44,26 @@ func (t *JsTransform) ProcessData(d data.JSON, outputChan chan data.JSON, killCh
 	if err != nil {
 		util.KillPipelineIfErr(fmt.Errorf("Unable to unmarshal JSON object for JS transformation: %s", err), killChan)
 	}
+	
+	t.vm.Set("data", objs)
 
-	var newObjs []map[string]interface{}
-
-	for _, obj := range objs {
-
-		t.vm.Set("obj", obj)
-
-		_, err := t.vm.Run(t.script)
-		if err != nil {
-			util.KillPipelineIfErr(fmt.Errorf("Runtime error in JS transformation: %s", err), killChan)
-		}
-
-		val, err := t.vm.Get("obj")
-		if err != nil {
-			util.KillPipelineIfErr(fmt.Errorf("Error executing JS transformation: %s", err), killChan)
-		}
-
-		newObj, err := val.Export()
-		if err != nil {
-			util.KillPipelineIfErr(fmt.Errorf("Error exporting JS value: %s", err), killChan)
-		}
-
-		newObjs = append(newObjs, newObj.(map[string]interface{}))
+	_, err = t.vm.Run(t.script)
+	if err != nil {
+		util.KillPipelineIfErr(fmt.Errorf("Runtime error in JS transformation: %s", err), killChan)
 	}
 
-	d, err = data.NewJSON(newObjs)
+	val, err := t.vm.Get("data")
+	if err != nil {
+		util.KillPipelineIfErr(fmt.Errorf("Error executing JS transformation: %s", err), killChan)
+	}
+
+	newObj, err := val.Export()
+	if err != nil {
+		util.KillPipelineIfErr(fmt.Errorf("Error exporting JS value: %s", err), killChan)
+	}
+
+	//d, err = data.NewJSON(newObjs)
+	d, err = data.NewJSON(newObj)
 	if err != nil {
 		util.KillPipelineIfErr(fmt.Errorf("Unable to marshal JS transformed object: %s", err), killChan)
 	}
