43e72cb9be4447862eb6549a4ca96233b57d8c57
Sam Hug <samuel.b.hug@gmail.com>
2016-05-16 14:54:39 -0700

    Fix JSONWriter

--- json_writer.go
+++ json_writer.go
@@ -9,21 +9,36 @@ import (
 
 type JSONWriter struct {
 	writer io.Writer
+	buffer []data.JSON
 }
 
 // NewJSONWriter returns a new JSONWriter wrapping the given io.Writer object
 func NewJSONWriter(w io.Writer) *JSONWriter {
 	return &JSONWriter{
 		writer: w,
+		buffer: make([]data.JSON, 0),
 	}
 }
 
 func (w *JSONWriter) ProcessData(d data.JSON, outputChan chan data.JSON, killChan chan error) {
-	_, err := w.writer.Write(d)
-	util.KillPipelineIfErr(err, killChan)
+	w.buffer = append(w.buffer, d)
 }
 
 func (w *JSONWriter) Finish(outputChan chan data.JSON, killChan chan error) {
+
+	_, err := w.writer.Write([]byte("["))
+	util.KillPipelineIfErr(err, killChan)
+
+	for _, d := range w.buffer {
+		_, err = w.writer.Write(d)
+		util.KillPipelineIfErr(err, killChan)
+
+		_, err = w.writer.Write([]byte(","))
+		util.KillPipelineIfErr(err, killChan)
+	}
+
+	_, err = w.writer.Write([]byte("]"))
+	util.KillPipelineIfErr(err, killChan)
 }
 
 func (w *JSONWriter) String() string {
